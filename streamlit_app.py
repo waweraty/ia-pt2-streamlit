5a48dd0377ff2b7a17fa844e58595e415b47f182
waweraty <35693847+waweraty@users.noreply.github.com>
2022-11-29 21:54:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ uploaded_file = st.file_uploader("Choose an image...")
 def predict_value(image,model):   
 	img_width, img_height = 512, 512
 	img = keras.preprocessing.image.load_img(image)
-	img = tf.image.central_crop(img, central_fraction=0.5)
-	img = tf.image.resize(img,[img_width, img_height])
+	#img = tf.image.central_crop(img, central_fraction=0.5)
+	#img = tf.image.resize(img,[img_width, img_height])
 	#st.image(img, caption='Input Image', use_column_width=True)
 	img = keras.utils.img_to_array(img)
 	img = np.expand_dims(img, axis = 0)
