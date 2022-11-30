59010513b5010e6ed4a2088b25abcbd70c81078b
waweraty <35693847+waweraty@users.noreply.github.com>
2022-11-29 18:09:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,9 @@ if uploaded_file is not None:
 		if int(pred_class[0][0].round()==0):
 			st.write('Image loaded is not suitable for the prediction model')
 			d = st.date_input("Please select the date of the Stage to load",
-			min_value=datetime.date(2012, 6, 9),max_value=datetime.date(2019, 10, 11))
+			datetime.date(2012, 6, 9),
+			min_value=datetime.date(2012, 6, 9),
+			max_value=datetime.date(2019, 10, 11))
 			t = st.time_input('Select the time for that date',datetime.time(0, 0))
 			st.write('The closest Stage to your date is:', d,t)
 		elif int(pred_class[0][0].round()==1):
