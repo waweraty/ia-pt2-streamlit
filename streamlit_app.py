89d8cfcf4be0cf3f898d108ffec86030de9b69c6
waweraty <35693847+waweraty@users.noreply.github.com>
2022-11-29 19:35:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ if uploaded_file is not None:
 			st.write('The closest Stage to your date is: ',s[0])
 	elif int(pred_class[0][0].round()==1):
 		st.write('Predicted Stage:')
-		st.write(uploaded_file.name)
-		st.write('Real:',0)
+		if uploaded_file.name in df['Filename']:
+			st.write('Real:',df.loc[uploaded_file.name])
 		st.write('Our model:',0)
 		st.write('VGG:',0)
